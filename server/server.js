e08a99b381a78b232a361f0e58ab2fe0e8958cce
Benoit Guerout <bguerout@xebia.fr>
2013-01-30 14:51:26 +0100

    Add server logs

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
 var express = require('express'),
-	app = express(),
-	db = require('mongojs').connect('mercenary', ['items']),
-	request = require('request').defaults({json: true}),
-	_ = require('underscore');
+    app = express(),
+    db = require('mongojs').connect('mercenary', ['items']),
+    request = require('request').defaults({json: true}),
+    _ = require('underscore');
 
 app.use(express.bodyParser());
 app.use(express.static(__dirname + '/../webapp'));
+app.use(express.favicon());
+app.use(express.logger('dev'));
 
 require('./routes/item')(app, db, _);
 require('./routes/node')(app, db, request, _);
 
 app.listen(3000);
+console.log("Server is listening for connections on port 3000...");
