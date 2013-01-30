4912546239b67aaeb2813cb18632e08d4f0761ad
Alexandre Dergham <adergham@xebia.fr>
2013-01-30 10:30:03 +0100

    première version de la recherche par elasticSearch

--- server/routes/item.js
+++ server/routes/item.js
@@ -1,4 +1,4 @@
-module.exports = function(app, db, _) {
+module.exports = function(app, db, _, http) {
 	var DEFAULT_PAGE = 1,
 		DEFAULT_RESULTS_PER_PAGE = 15;
 
@@ -9,7 +9,7 @@ module.exports = function(app, db, _) {
 	});
 
 	app.get('/item', function(req, res) {
-		var search = {},
+		var search = [],
 			page = req.query['page'] || DEFAULT_PAGE,
 			results_per_page = req.query['results_per_page'] || DEFAULT_RESULTS_PER_PAGE;
 
@@ -17,16 +17,47 @@ module.exports = function(app, db, _) {
 		_.each(req.query, function(queryParam, queryName) {
 			console.log('- ' + queryName + ' like "' + queryParam + '"');
 			if(queryName === 'page' || queryName === 'results_per_page') return;
-			search[queryName] = new RegExp(queryParam, 'i');
+			if(queryParam==='') return;
+            search.push(queryName+":"+queryParam);
 		});
 
-		db.items.find(search).sort({title: 1}).skip((page-1)*results_per_page).limit(results_per_page, function(err, items) {
-			res.send({
-				page: page,
-				results_per_page: results_per_page,
-				results: items
-			});
-		});
+        var pathParam = "/mongoidx/_search?from="+ (page-1)*results_per_page +"&size="+ results_per_page
+        for(var i=0; i<search.length; i++){
+            if(i==0){
+                pathParam = pathParam + "&q="+search[i];
+            }else {
+                pathParam = pathParam + "%20AND%20"+search[i];
+            }
+
+        }
+        console.log(pathParam)
+        var requestOptions = {
+            host:"localhost",
+            port:"9200",
+            path:pathParam,
+            method: 'GET'
+        }
+
+        var request  = http.request(requestOptions, function(response) {
+            response.on('data', function(data) {
+                var jsonData =  JSON.parse(data)
+                var  items = []
+                for (var i=0;i<jsonData.hits.hits.length;i++) {
+                    items.push(jsonData.hits.hits[i]._source);
+                }
+                res.send({
+                    page: page,
+                    results_per_page: results_per_page,
+                    results: items
+                });
+            });
+        }).on('error', function(e) {
+                console.log("Got error: " + e.message);
+        });
+
+
+        request.end();
+
 	});
 
 	app.get('/item/:id', function(req, res) {
--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 var express = require('express'),
 	app = express(),
+    http = require('http'),
 	db = require('mongojs').connect('mercenary', ['items']),
 	_ = require('underscore');
 
 app.use(express.bodyParser());
 app.use(express.static(__dirname + '/../webapp'));
 
-require('./routes/item')(app, db, _);
+require('./routes/item')(app, db, _, http);
 
 app.listen(3000);
