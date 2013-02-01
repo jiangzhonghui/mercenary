630bd70705b2174dd53450eefa8da725b6533a7a
Benoit Guerout <bguerout@xebia.fr>
2013-02-01 17:15:43 +0100

    Remove 'git checkout' from script

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -3,7 +3,6 @@ REPO_DIR=/home/mercenary/mercenary
 LOG_DIR=$REPO_DIR/logs
 
 pushd $REPO_DIR/server
-git checkout release
 nohup supervisor -n exit server.js </dev/null >> $LOG_DIR/nohup_server.log 2>&1 &
 echo $$ > $LOG_DIR/nodejs.pid
 
