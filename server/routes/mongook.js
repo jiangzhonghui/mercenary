f80050dd557c1e8ad80a0ed959085c2ab887c4ba
Florent DUVEAU <florent.duveau@gmail.com>
2013-02-04 20:01:58 +0100

    add log

--- server/routes/mongook.js
+++ server/routes/mongook.js
@@ -1,14 +1,14 @@
 var mongoose = require('mongoose');
 
-module.exports = function () {
-	var dbURL = 'mongodb://localhost/mercenary';
-	mongoose.connect(dbURL);
-	mongoose.set('debug', true);
+module.exports = function (logger) {
+    var dbURL = 'mongodb://localhost/mercenary';
+    mongoose.connect(dbURL);
+    mongoose.set('debug', true);
 
-	var db = mongoose.connection;
+    var db = mongoose.connection;
 
-	db.on('error', console.error.bind(console, 'connection error:'));
-	db.once('open', function callback() {
-	    console.log('connected to mongodb !');
-	});
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function callback() {
+        logger.info('connected to mongodb !');
+    });
 };
