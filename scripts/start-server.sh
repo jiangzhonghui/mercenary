17a767143dcd0ae35f18263725441ad6dc01a0b6
Benoit Guerout <bguerout@xebia.fr>
2013-02-04 23:45:03 +0100

    Add NODE_ENV into EC2 script

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -2,6 +2,8 @@
 REPO_DIR=/home/mercenary/mercenary
 LOG_DIR=/home/mercenary/logs
 
+export NODE_ENV=prod
+
 pushd $REPO_DIR/server
 npm install
 nohup supervisor -n exit server.js </dev/null > $LOG_DIR/nohup_server.log 2>&1 &
