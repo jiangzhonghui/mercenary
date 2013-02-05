1a91fd7b7d6e33e3311264b42355c4a0ae2d6837
Benoit Guerout <bguerout@xebia.fr>
2013-02-05 11:46:41 +0100

    Fix LOG_DIR path

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-REPO_DIR=/home/mercenary/mercenary
 LOG_DIR=/home/mercenary/logs
 
 export NODE_ENV=prod
 
-pushd $REPO_DIR/server
+pushd /home/mercenary/mercenary/server
 nohup supervisor -q -n error  server.js </dev/null > $LOG_DIR/nohup_server.log 2>&1 &
 echo $$ > $LOG_DIR/nodejs.pid
 
