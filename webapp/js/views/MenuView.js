a6e714e189ca7f2b4e891e1ac58da4918cf5ae48
sophietk <sophie.tk@gmail.com>
2013-02-05 23:11:31 +0100

    Extract LoginView from menu (view, css)

--- webapp/js/views/MenuView.js
+++ webapp/js/views/MenuView.js
@@ -1,18 +1,9 @@
-define(['text!templates/menu.mustache', 'models/LoginModel'], function (template, LoginModel) {
+define(['text!templates/menu.mustache'], function (template) {
     return Backbone.View.extend({
         el: '#menu',
         template: Hogan.compile(template),
         events: {
-            'click li': 'changeMenu',
-            'click button.in': 'login',
-            'keypress input': 'loginKb',
-            'click button.out': 'logout'
-        },
-        initialize: function () {
-            this.model = new LoginModel();
-            this.listenTo(this.model, 'sync error', this.renderLogin);
-            if (document.cookie) this.model.fetch();
-            else this.renderLogin();
+            'click li': 'changeMenu'
         },
         render: function () {
             this.$('ul').html(this.template.render());
@@ -25,27 +16,6 @@ define(['text!templates/menu.mustache', 'models/LoginModel'], function (template
         highlight: function (index) {
             this.$('li').removeClass('active');
             if (index) this.$('li:nth-child(' + index + ')').addClass('active');
-        },
-        renderLogin: function () {
-            if (this.model.get('name'))
-                $('#login').html('<span class="name">' + this.model.get('name') + '</span><button class="out blue-button">logout</button>');
-            else
-                $('#login').html('<input type="text" placeholder="Nom"/><button class="in blue-button">login</button>');
-        },
-        loginKb: function (event) {
-            if (event.which === 13)
-                this.login(event);
-        },
-        login: function (event) {
-            event.preventDefault();
-            var name = $('#login').find('input').val();
-            if (name)
-                this.model.save({name: name});
-        },
-        logout: function (event) {
-            event.preventDefault();
-            this.model.sync('delete', this.model);
-            this.model.clear();
         }
     });
 });
