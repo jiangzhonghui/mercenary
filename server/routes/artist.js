250e6f5873f51287e939bd68482e57f25dc3dd8f
Jean-Eudes <jeudes7@gmail.com>
2013-02-06 15:21:21 +0100

    add image

--- server/routes/artist.js
+++ server/routes/artist.js
@@ -20,8 +20,8 @@ module.exports = function (app, db, logger) {
         if (page && results_per_page) {
             ArtistQuery.pagination(search, page, results_per_page, function (artists) {
                 _.each(artists, function(artist) {
-                     var i = Math.floor(Math.random() * 500);
-                     artist.image = "http://cdn.7static.com/static/img/artistimages/00/000/000/0000000" + i + "_150.jpg";
+                     var i = Math.floor(Math.random() * 500) + 100;
+                     artist.image = "http://cdn.7static.com/static/img/artistimages/00/000/000/0000000" + i + "_50.jpg";
                 })
                 res.send({
                     page: page,
@@ -41,7 +41,7 @@ module.exports = function (app, db, logger) {
     app.get('/artist/:id', function (req, res) {
         ArtistQuery.findOne(req.param('id'), function (artist) {
             artist.image = "toto";
-            url = "http://api.7digital.com/1.2/artist/details?artistId=" + artist.artist_7digitalid + "&imageSize=100&oauth_consumer_key=7dqm93aabzws";
+            url = "http://api.7digital.com/1.2/artist/details?artistId=" + artist.artist_7digitalid + "&imageSize=50&oauth_consumer_key=7dqm93aabzws";
             request(url, function (error, response, body) {
               if (!error && response.statusCode == 200) {
                 XmlParser(body, function(error, result){
