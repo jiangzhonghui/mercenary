3f8e42b4d50827dfdce3c8289cf6614ae6fa83d4
yamsellem <amsellem.yves@gmail.com>
2013-02-06 11:43:30 +0100

    server, extract methods

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,29 +1,38 @@
 module.exports = function(app, db, logger) {
+
 	app.post('/user', function(req, res) {
-		if(req.body.city && req.body.username) {
-			db.users.findOne({mail: req.body.mail}, function(err, user) {
-				if(!user) {
-					db.users.save(req.body, function() {
-						res.cookie('rememberme', req.body, { maxAge: 900000, httpOnly: false });
-						res.send(req.body, 201);
-					});
-				} else {
-					res.statusCode = 401;
-					res.send();
-				}
-			});
-		} else {
-			db.users.findOne({mail: req.body.mail}, function(err, user) {
-				if(user) {
-					res.cookie('rememberme', user, { maxAge: 900000, httpOnly: false });
-					res.send(user);
-				} else {
-					res.statusCode = 401;
-					res.send();
-				}
-			});
-		}
+		if(req.body.city && req.body.username)
+			createAccount(req, res);
+		else
+			connectToAccount(req, res);
 	});
+
+	var unauthorized = function(res) {
+		res.statusCode = 401;
+		res.send();
+	};
+
+	var createAccount = function(req, res) {
+		db.users.findOne({mail: req.body.mail}, function(err, user) {
+			if(user)
+				unauthorized();
+
+			db.users.save(req.body, function() {
+				res.cookie('rememberme', req.body, { maxAge: 900000, httpOnly: false });
+				res.send(req.body, 201);
+			});			
+		});
+	};
+
+	var connectToAccount = function(req, res) {
+		db.users.findOne({mail: req.body.mail}, function(err, user) {
+			if(!user)
+				unauthorized();
+
+			res.cookie('rememberme', user, { maxAge: 900000, httpOnly: false });
+			res.send(user);
+		});
+	};
 
 	app.delete('/user', function(req, res) {
 		res.clearCookie('rememberme');
